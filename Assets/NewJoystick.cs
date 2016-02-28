70c21da4e7666cd1c48bcf24bcfc812ef4025442
Your Name <no@no.no>
2016-02-27 21:19:45 -0800

    Try rotating camera

---
+++ Assets/NewJoystick.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewJoystick : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Rotate(Vector3.right * Time.deltaTime);
+        transform.Rotate(Vector3.up * Time.deltaTime, Space.World);
+    }
+}
